88611c41427b0a6b719cf6caf8fa5e32826fa617
1900880 <1900880@uad.ac.uk>
2020-07-30 15:45:49 +0100

    Week 5

--- Week5/CMP105App/Level.cpp
+++ Week5/CMP105App/Level.cpp
@@ -4,9 +4,11 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 {
 	window = hwnd;
 	input = in;
-
 	// initialise game objects
-
+	zombTexture.loadFromFile("gfx/animZombie.png");
+	zombie.setSize(sf::Vector2f(55, 108));
+	zombie.setPosition(100, 100);
+	zombie.setTexture(&zombTexture);
 }
 
 Level::~Level()
@@ -23,14 +25,14 @@ void Level::handleInput(float dt)
 // Update game objects
 void Level::update(float dt)
 {
-
+	zombie.update(dt);
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
-
+	window->draw(zombie);
 	endDraw();
 }
 
